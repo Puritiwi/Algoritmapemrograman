bc0dd35e88f03f92f84dcb957e8504f6c4d118b2
Puritiwi <136144515+Puritiwi@users.noreply.github.com>
2023-06-10 12:55:11 +0700

    Update Algo_latihan1.py
    Menambahkan source code tentang penilaian KHS mahasiswa

--- Algo_latihan1.py
+++ Algo_latihan1.py
@@ -1,1 +1,33 @@
-maaf belum diisi,nantikan acara selanjutnya
+# Setting Variabel
+nim   = "20122072"
+nama  = "puri pratiwi"
+kelas = "2A"
+prodi = "informatika"
+
+#Setting Variabel nilai
+bhs_indo =76
+bhs_ing  =70
+pd       =90
+mtk      =65
+kal1     =67
+so       =75
+
+# Perhitungan
+rata = (bhs_indo+bhs_ing+pd+mtk+kal1+so)/6
+
+# cetak hasil
+print("-----------------------")
+print("NIM Anda         :",nim)
+print("Nama Lengkap Anda:",nama)
+print("Kelas            :",kelas)
+print("Program Studi    :",prodi)
+print("-------------------------")
+print("1.Bahasa Indonesia:",bhs_indo)
+print("2.Bahasa Inggris :",bhs_ing)
+print("3.Pemrograman Dasar   :",pd)
+print("4.Matematika         :",mtk)
+print("5.Kalkulus 1        :",kal1)
+print("6.Sistem Operasi      :",so)
+print("-------------------------")
+print("Rata - rata         :",rata)
+print("--------------------------")
