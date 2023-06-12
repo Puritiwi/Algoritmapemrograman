fb91897ccfc3d28cd6f75941c6ec52caba48ae5f
Puritiwi <136144515+Puritiwi@users.noreply.github.com>
2023-06-12 12:36:14 +0700

    Update Algo_Latihan2.py
    tugas1

--- Algo_Latihan2.py
+++ Algo_Latihan2.py
@@ -1,1 +1,16 @@
-maaf belum diisi 
+# Setting Variabel Nilai 
+a = 10 
+b = 20 
+c = 50
+
+# Perhitungan 
+hasil = ((a * b) * (b + c)) / (a + b + c)
+
+# Hasil Perhitungan 
+print("----------------------")
+print("Masukkan nilai a:", a)
+print("Masukkan nilai b:", b)
+print("Masukkan nilai c:", c)
+print("----------------------")
+print("Hasil perhitungan: ", hasil)
+print("----------------------")
