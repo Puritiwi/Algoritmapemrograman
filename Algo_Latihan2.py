d6a0fb83fc2e32877fd2e23f9a7501fef0e8d020
Puritiwi <136144515+Puritiwi@users.noreply.github.com>
2023-06-15 08:22:52 +0700

    Update Algo_Latihan2.py

--- Algo_Latihan2.py
+++ Algo_Latihan2.py
@@ -14,3 +14,22 @@ print("Masukkan nilai c:", c)
 print("----------------------")
 print("Hasil perhitungan: ", hasil)
 print("----------------------")
+
+
+# Setting Variabel Nilai 
+p = 10 
+l = 20 
+t = 50
+
+# Perhitungan 
+hasil =p*l*t
+# Hasil Perhitungan
+print("----------------------")
+print("Masukkan nilai p:", p)
+print("Masukkan nilai l:", l)
+print("Masukkan nilai t:", t)
+print("----------------------")
+print("Hasil perhitungan: ", hasil)
+print("----------------------")
+
+
