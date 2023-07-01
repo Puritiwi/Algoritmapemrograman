5ed82b09a2e3a8e5d54744080b0fcf252ea742a2
Puritiwi <136144515+Puritiwi@users.noreply.github.com>
2023-07-01 13:06:51 +0700

    Create latihan6_tipedatalanjutan.py

---
+++ latihan6_tipedatalanjutan.py
@@ -0,0 +1,31 @@
+# Setting Variabel Identitas
+nim   = input("Masukkan NIM   :")
+nama  = input("Masukkan Nama  :")
+kelas = input("Masukkan Kelas :")
+prodi = input("Masukkan Prodi :")
+
+# Perhitungsn
+phi=3.14
+r = int(input("Masukkan Panjang Jari-Jari Lingkaran: "))
+
+luas = phi*(r*r)
+keliling = 2*phi*r
+# Setting Variabel
+kalimat = ["UNISS","di","belajar","pada","Mahasiswa","Pemrograman","ruang","lab","Algoritma","semester","topik","Data","tipe","dan","dengan","Batang",2]
+
+print("------------------------------------------------")
+print("              OPERASI ARITMATIKA                ")
+print("------------------------------------------------")
+print("nim    :%s"%(nim))
+print("nama  :%s"%(nama))
+print("kelas:%s"%(kelas))
+print("prodi:%s"%(prodi))
+print("------------------------------------------------")
+print("                   LINGKARAN                    ")
+print("------------------------------------------------")
+print("Luas lingkaran adalah :%d"%(luas))
+print("Keliling lingkaran adalah :%d"%(keliling))
+print("-------------------------------------------------")
+print("                      KALIMAT                    ")
+print("-------------------------------------------------")
+print(kalimat[4],kalimat[9],kalimat[16],kalimat[2],kalimat[8],kalimat[5],kalimat[14],kalimat[10],kalimat[12],kalimat[11],kalimat[1],kalimat[6],kalimat[7],kalimat[1],kalimat[0],kalimat[15])
